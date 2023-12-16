46df6910963cfae9cd01b975ac5fa39a5f88a84d
tcs <tcs211@gmail.com>
2023-12-16 12:38:01 +0800

    update

--- app.mjs
+++ app.mjs
@@ -28,6 +28,7 @@ app.get('/', (req, res) => {
   <html>
   <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
   <title>No1Security</title>
   </head>
@@ -82,6 +83,7 @@ app.get('/register', (req, res) => {
   <html>
   <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
   <title>No1Security註冊</title>
   </head>
